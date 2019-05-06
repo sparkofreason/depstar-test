f7414eff3d17521deb48f379d3243dcc82f95615
dave <dave@computesoftware.com>
2019-05-06 12:19:58 -0700

    Init commit

---
+++ test/compute/uberjar_test_test.clj
@@ -0,0 +1,7 @@
+(ns compute.uberjar-test-test
+  (:require [clojure.test :refer :all]
+            [compute.uberjar-test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
